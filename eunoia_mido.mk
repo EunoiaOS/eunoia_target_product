6cc1d0de0816ab0cbf8ba564d29aa43641bab065
BayerischeMotorenWerke <syaifulamri199@gmail.com>
2026-01-12 06:25:55 +0700

    Mido: Build maintainer overlay
    Change-Id: Id7321fd75a8dc8288a530912e1d2bf05c61ee60d

--- eunoia_mido.mk
+++ eunoia_mido.mk
@@ -29,6 +29,7 @@ $(call inherit-product, vendor/os/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_FACE_UNLOCK_SUPPORTED := true
 EUNOIA_MAINTAINER := BayerischeMotorenWerke
+PRODUCT_PACKAGES += EunoiaMaintainerMido
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
